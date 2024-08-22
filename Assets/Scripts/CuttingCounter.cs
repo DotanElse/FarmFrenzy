f9a7059e2b3b16f507471d6cd0ba79320a8f33dc
DotanElse <dotanelse@gmail.com>
2024-08-22 13:08:46 +0300

    Added UI cutting

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
+    public event EventHandler<OnProgressChangeArgs> OnProgressChange;
+    public class OnProgressChangeArgs : EventArgs
+    {
+        public float normalizedProgress;
+    }
+    private int cuttingProgress;
     [SerializeField] private CuttingRecipeSO[] recipes;
     public override void Interact(Player player)
     {
@@ -11,6 +18,10 @@ public class CuttingCounter : BaseCounter
         {
             if(player.HasKitchenObject())
             {
+                cuttingProgress = 0;
+                OnProgressChange?.Invoke(this,  new OnProgressChangeArgs{
+                normalizedProgress = cuttingProgress
+                });
                 player.GetKitchenObject().SetKitchenObjectParent(this);
             }
         }
@@ -26,18 +37,36 @@ public class CuttingCounter : BaseCounter
     {
         if(HasKitchenObject())
         {
-            Debug.Log("Happening");
             KitchenObjectSO output = GetCutResult(GetKitchenObject().GetKitchenObjectSO());
             if(output == null)
                 return;
-            GetKitchenObject().DestroySelf();
-            KitchenObject.SpawnKitchenObject(output, this);
+            cuttingProgress++;
+            int requiredCuts = GetRequiredCuts(GetKitchenObject().GetKitchenObjectSO());
+            OnProgressChange?.Invoke(this,  new OnProgressChangeArgs{
+                normalizedProgress = (float)cuttingProgress/requiredCuts
+            });
+
+            if(cuttingProgress == requiredCuts)
+            {
+                GetKitchenObject().DestroySelf();
+                KitchenObject.SpawnKitchenObject(output, this);
+            }
         }
     }
 
     private bool HasRecipe(KitchenObjectSO item)
     {
         return GetCutResult(item) == null;
+    }
+
+    private int GetRequiredCuts(KitchenObjectSO item)
+    {
+        foreach (CuttingRecipeSO recipe in recipes)
+        {
+            if(recipe.input == item)
+                return recipe.cutsRequired;
+        }
+        return 0;
     }
 
     private KitchenObjectSO GetCutResult(KitchenObjectSO item)
---
+++ Assets/Scripts/ProgressBarUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private Image barImage;
+    [SerializeField] private CuttingCounter cuttingCounter; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("init");
+        cuttingCounter.OnProgressChange += setBarProgress;
+        Hide();
+        barImage.fillAmount = 0f;
+    }
+
+    void setBarProgress(object sender, CuttingCounter.OnProgressChangeArgs e)
+    {
+        if(e.normalizedProgress != 1f)
+        {
+            barImage.fillAmount = e.normalizedProgress;
+            Debug.Log($"progressed, {e.normalizedProgress}");
+            Show();
+        }
+        else{
+            Hide();
+        }
+    }
+    void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    
+}
